297c209bab5b06b88d1346ad1f765a04de7954ee
ZengHL <1057890843@qq.com>
2021-05-28 11:44:15 +0800

    Index

---
+++ pages/index/index.js
@@ -0,0 +1,75 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    resCode: '',
+    background: ['/images/ad1.jpg', '/images/ad2.jpg', '/images/ad3.jpg', '/images/ad4.jpg'],
+  },
+
+  scanCode: function(){
+    wx.scanCode({
+      onlyFromCamera: false,
+      success: (res) => {
+        this.setData({ resCode: res.result })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
