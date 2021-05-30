d4cc3939b6418f3426c9443c16bcd0dab386201d
ZengHL <1057890843@qq.com>
2021-05-30 16:55:01 +0800

    Modify images url, Modify cart page.

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,107 @@
 // pages/cart/cart.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        "carts": [{
+                "id": "102",
+                "num": "4",
+                "photo_little": "cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/12pm.jpg",
+                "pid": "8",
+                "price": "10099.00",
+                "pro_name": "Apple iPhone 12 Pro Max (A2412) 256GB 海蓝色 支持移动联通电信5G 双卡双待手机"
+            },
+            {
+                "uid": "5",
+                "id": "103",
+                "num": "1",
+                "photo_little": "cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/laptop/laptop1.jpg",
+                "pid": "6",
+                "price": "6899.00",
+                "pro_name": "联想(Lenovo)YOGA 14S锐龙R7 2021八核2.8K屏超轻薄笔记本电脑 设计游戏超极本 标配：8核R7 5800H 16G+512G固态 100%sRGB高色域 轻奢灰",
+                "uid": "5"
+            },
+            {
+                "id": "104",
+                "num": "1",
+                "photo_little": "cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/daikin/daikin1.jpg",
+                "pid": "324",
+                "price": "4299.00",
+                "pro_name": "大金空调 14-19㎡适用 新三级能效 大1.5匹 变频 制冷 家用 壁挂式 以旧换新 FTXJ336WC-W",
+                "uid": "5"
+            }
+        ],
+    },
 
-  },
+    bindCheckbox: function(e) {
+        var _index = parseInt(e.currentTarget.dataset.index);
+        var _selected = this.data.carts[_index].selected;
+        var carts = this.data.carts;
+        carts[_index].selected = !_selected;
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+        this.setData({
+            carts: carts
+        })
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        console.log(this.data.carts)
+        this.setData({
+            carts: this.data.carts
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  }
+    }
 })
--- pages/product/detail.js
+++ pages/product/detail.js
@@ -6,20 +6,20 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrls: ['cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/12pm.jpg','cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/1.jpg',
-    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/2.jpg',
-    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/3.jpg',
-    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/4.jpg',
-    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/5.jpg',
-    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/6.jpg',
-    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/7.jpg']
+    imgUrls: ['cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/12pm.jpg','cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/1.jpg',
+    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/2.jpg',
+    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/3.jpg',
+    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/4.jpg',
+    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/5.jpg',
+    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/6.jpg',
+    'cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/7.jpg']
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var content = '<p style="margin: 1.12em 0px; padding: 0px; line-height: 1.4; color: rgb(64, 64, 64); font-family: tahoma, arial, 宋体, sans-serif; font-size: 14px; text-align: center;"><img src="cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/Details1.jpg" alt="" width="100%" /><img src="cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/Details2.jpg" alt="" width="100%" /><br /></p>';
+    var content = '<p style="margin: 1.12em 0px; padding: 0px; line-height: 1.4; color: rgb(64, 64, 64); font-family: tahoma, arial, 宋体, sans-serif; font-size: 14px; text-align: center;"><img src="cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/Details1.jpg" alt="" width="100%" /><img src="cloud://cloud-test-0govni141f2dc556.636c-cloud-test-0govni141f2dc556-1305944833/images/product_images/iP12pm/Details2.jpg" alt="" width="100%" /><br /></p>';
     WxParse.wxParse('content', 'html', content, this, 0);
   },
 
